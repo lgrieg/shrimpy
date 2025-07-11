a3e48675cfe05efedd936fbc7dc4875b0236f022
Lada <nastnd@gmail.com>
2025-07-11 21:28:28 +0300

    change xlsx parcer

--- src/trainingParser.js
+++ src/trainingParser.js
@@ -1,10 +1,12 @@
+import * as XLSX from 'https://cdn.sheetjs.com/xlsx-0.20.0/package/xlsx.mjs';
+
 export function parseExcel(file, callback) {
   const reader = new FileReader();
-  reader.onload = function(e) {
+  reader.onload = function (e) {
     const data = new Uint8Array(e.target.result);
     const workbook = XLSX.read(data, { type: "array" });
-    const firstSheet = workbook.Sheets[workbook.SheetNames[0]];
-    const json = XLSX.utils.sheet_to_json(firstSheet);
+    const sheet = workbook.Sheets[workbook.SheetNames[0]];
+    const json = XLSX.utils.sheet_to_json(sheet);
     callback(json);
   };
   reader.readAsArrayBuffer(file);
