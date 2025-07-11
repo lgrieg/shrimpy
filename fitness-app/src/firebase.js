116ca165d4614dcf02b11c43b8941a79461c38e7
Lada <nastnd@gmail.com>
2025-07-11 20:53:38 +0300

    add firebase

--- fitness-app/src/firebase.js
+++ fitness-app/src/firebase.js
@@ -1,17 +1,24 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-app.js";
-import { getDatabase, ref, set, get, child } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-database.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "your-app.firebaseapp.com",
-  databaseURL: "https://your-app.firebaseio.com",
-  projectId: "your-app",
-  storageBucket: "your-app.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:abcdef123456"
+  apiKey: "AIzaSyAPEX9utX9Bqj3lcvZsOdJnprc3BBtXvcA",
+  authDomain: "shrimpy-710e2.firebaseapp.com",
+  projectId: "shrimpy-710e2",
+  storageBucket: "shrimpy-710e2.firebasestorage.app",
+  messagingSenderId: "896165241896",
+  appId: "1:896165241896:web:ded0978006c6227958011d",
+  measurementId: "G-DK6XKTJ9LT"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
+const analytics = getAnalytics(app);
+
 
 export { db, ref, set, get, child };
